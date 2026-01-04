9045b88146c65fc9183d4c72d69674e2cd471d4f
jeffassis <jeff-assis@hotmail.com>
2026-01-04 11:01:55 -0300

    first commit

---
+++ src/test/java/gw/lion/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gw.lion.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
